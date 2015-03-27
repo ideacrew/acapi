d1e0771ab445fac478e30fb55d5afd536dc7ac2a
Ken Lu <ken@seeice.com>
2015-03-27 09:39:03 -0400

    added another version of corp_logger

--- lib/acapi/publishers/corp_logger.rb
+++ lib/acapi/publishers/corp_logger.rb
@@ -1,11 +1,37 @@
 module Acapi
   module Publishers
     module CorpLogger
+      extend ActiveSupport::Concern
 
-      def corp_logger(msg)
+      def corp_logger_notification(msg)
         Acapi::Publishers.broadcast_event("logger", plan: msg)
       end
 
+      module ClassMethods
+        def corp_logger_on(method_name, msg)
+          new_method = "new_" << method_name.to_s
+          old_method = "old_" << method_name.to_s
+
+          define_method new_method do
+            self.send(old_method)
+            corp_logger_notification(msg)
+          end
+          
+          alias_method old_method, method_name 
+          alias_method method_name, new_method
+
+          #flag = false
+          #define_singleton_method :method_added do |name|
+          #    puts "add #{name}"
+          #  if name == method_name && flag == false 
+          #    puts "add #{name}"
+          #    flag = true
+          #    alias_method old_method, method_name 
+          #    alias_method method_name, new_method
+          #  end 
+          #end 
+        end
+      end 
 
       #def prepare_notifications(namespace, family)
 
