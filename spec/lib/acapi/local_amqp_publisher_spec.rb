abb6e689e01f58513ee54ace93dd628e2ec94b96
Trey Evans <lewis.r.evans@gmail.com>
2015-03-29 16:17:11 -0400

    Behaviour for event logging.
    Figured out how we're going to map the various behaviours to the event
    logs.

--- spec/lib/acapi/local_amqp_publisher_spec.rb
+++ spec/lib/acapi/local_amqp_publisher_spec.rb
@@ -1,27 +1,40 @@
 require 'spec_helper'
 
-describe "on initialization" do
-  let(:session) { instance_double("Bunny::Session") }
-  let(:channel) { instance_double("Bunny::Channel") }
-  let(:queue) { instance_double("Bunny::Queue") }
-  let(:forwarding_queue_name) { "acapi.events.local" }
+describe Acapi::LocalAmqpPublisher do
+  describe "on initialization" do
+    let(:session) { instance_double("Bunny::Session") }
+    let(:channel) { instance_double("Bunny::Channel") }
+    let(:queue) { instance_double("Bunny::Queue") }
+    let(:forwarding_queue_name) { "acapi.events.local" }
 
-  before :each do
-     allow(Bunny).to receive(:new).and_return(session)
-     allow(session).to receive(:start)
-     allow(session).to receive(:create_channel).and_return(channel)
-     allow(channel).to receive(:queue).with(forwarding_queue_name, {:persistent => true}).and_return(queue)
+    before :each do
+      allow(Bunny).to receive(:new).and_return(session)
+      allow(session).to receive(:start)
+      allow(session).to receive(:create_channel).and_return(channel)
+      allow(channel).to receive(:queue).with(forwarding_queue_name, {:persistent => true}).and_return(queue)
+    end
+
+    it "should establish a connection to the local broker" do
+      expect(Bunny).to receive(:new).and_return(session)
+      expect(session).to receive(:start)
+      expect(session).to receive(:create_channel).and_return(channel)
+      ::Acapi::LocalAmqpPublisher.boot!
+    end
+
+    it "should establish a persistent queue on the local broker" do
+      expect(channel).to receive(:queue).with(forwarding_queue_name, {:persistent => true}).and_return(queue)
+      ::Acapi::LocalAmqpPublisher.boot!
+    end
   end
 
-  it "should establish a connection to the local broker" do
-     expect(Bunny).to receive(:new).and_return(session)
-     expect(session).to receive(:start)
-     expect(session).to receive(:create_channel).and_return(channel)
-     ::Acapi::LocalAmqpPublisher.boot!
+  describe "which publishes messages" do
+    it "publishes with a routing key the same as the event name, just stripped of 'acapi.'"
+    it "creates the submitted_at key from the finished_at property of the event"
+    it "uses the :body property of the event to populate the body of the message"
+    it "uses all other event properties as headers"
   end
 
-  it "should establish a persistent queue on the local broker" do
-    expect(channel).to receive(:queue).with(forwarding_queue_name, {:persistent => true}).and_return(queue)
-    ::Acapi::LocalAmqpPublisher.boot!
+  describe "that can support unicorn" do
+    it "supports reconnection for after_fork"
   end
 end
