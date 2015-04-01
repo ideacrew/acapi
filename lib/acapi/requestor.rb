7772fdfacb69c27a4829381b88b307ffdba76008
Trey Evans <lewis.r.evans@gmail.com>
2015-04-01 15:32:13 -0400

    Still working on connection failures.

--- lib/acapi/requestor.rb
+++ lib/acapi/requestor.rb
@@ -49,20 +49,19 @@ module Acapi
     end
 
     def self.disable!
-      if defined?(@@instance) && !@instance.nil?
+      if defined?(@@instance) && !@@instance.nil?
         @@instance.disconnect!
       end
       @@instance = DoNothingRequestor.new
     end
 
     def self.boot!(app_id, uri)
-      if defined?(@@instance) && !@instance.nil?
+      if defined?(@@instance) && !@@instance.nil?
         @@instance.disconnect!
       end
       conn = Bunny.new(uri, :heartbeat => 1)
       conn.start
       slug_channel = conn.create_channel # We need a slug default channel
-      raise slug_channel.inspect
       @@instance = ::Acapi::Requestor::AmqpRequestor.new(app_id, uri, conn, slug_channel)
     end
 
