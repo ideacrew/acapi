808c65a823f9c1b2f17dce9128245c98796b243f
Trey Evans <lewis.r.evans@gmail.com>
2015-04-01 16:03:02 -0400

    Still working on connection failures.

--- lib/acapi/amqp/requestor.rb
+++ lib/acapi/amqp/requestor.rb
@@ -10,25 +10,24 @@ module Acapi
 
       def request(properties, payload, timeout = 15)
         delivery_info, r_props, r_payload = [nil, nil, nil]
-        @connection.with_channel do |channel|
-          temp_queue = channel.queue("", :exclusive => true)
-          channel.prefetch(1)
-          request_exchange = channel.direct(Rails.application.config.acapi.remote_request_exchange, :durable => true)
-          request_exchange.publish(payload, properties.dup.merge({ :reply_to => temp_queue.name, :persistent => true }))
-          delivery_info, r_props, r_payload = [nil, nil, nil]
-          begin
-            Timeout::timeout(timeout) do
-              temp_queue.subscribe({:manual_ack => true, :block => true}) do |di, prop, pay|
-                delivery_info, r_props, r_payload = [di, prop, pay]
-                channel.acknowledge(di.delivery_tag, false)
-                throw :terminate, "success"
-              end
+        channel = @connection.create_channel
+        temp_queue = channel.queue("", :exclusive => true)
+        channel.prefetch(1)
+        request_exchange = channel.direct(Rails.application.config.acapi.remote_request_exchange, :durable => true)
+        request_exchange.publish(payload, properties.dup.merge({ :reply_to => temp_queue.name, :persistent => true }))
+        delivery_info, r_props, r_payload = [nil, nil, nil]
+        begin
+          Timeout::timeout(timeout) do
+            temp_queue.subscribe({:manual_ack => true, :block => true}) do |di, prop, pay|
+              delivery_info, r_props, r_payload = [di, prop, pay]
+              channel.acknowledge(di.delivery_tag, false)
+              throw :terminate, "success"
             end
-          ensure
-            temp_queue.delete
           end
-          [delivery_info, r_props, r_payload]
+        ensure
+          temp_queue.delete
         end
+        [delivery_info, r_props, r_payload]
       end
 
     end
