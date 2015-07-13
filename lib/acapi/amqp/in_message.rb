6264f5539602fff8f779448519e32d94d71a9380
Trey Evans <lewis.r.evans@gmail.com>
2015-07-13 12:22:20 -0400

    Fixing interpretation of inbound times.

--- lib/acapi/amqp/in_message.rb
+++ lib/acapi/amqp/in_message.rb
@@ -14,20 +14,46 @@ module Acapi
         rk_name = extract_event_name(@delivery_info)
         msg_id = @props.message_id # Generate guid if not provided
         msg_id ||= SecureRandom.uuid.gsub("-", "")
-        stime = extract_start_time(properties)
+        stime = extract_time_value(properties)
         payload = extract_payload(properties, @body)
         [rk_name, stime, stime, msg_id, payload]
       end
 
+
+      def extract_time_value(props)
+        time_from_submitted = extract_start_time(props)
+        time_from_timestamp = extract_timestamp_prop(props)
+        if !time_from_submitted.nil?
+          return time_from_submitted
+        elsif !time_from_timestamp.nil?
+          return time_from_timestamp
+        end
+        Time.now
+      end
+
+      def extract_timestamp_prop(props)
+        if props.has_key?(:timestamp)
+          return(Time.at(props[:timestamp].to_i) rescue nil)
+        elsif props.has_key?("timestamp")
+          return(Time.at(props["timestamp"].to_i) rescue nil)
+        end
+      end
+
       def extract_start_time(props)
         headers = props[:headers] || {}
-        ts_val = Time.now
         if headers.has_key?("submitted_timestamp")
-          ts_val = headers["submitted_timestamp"]
+          return(parse_submitted_at(headers["submitted_timestamp"]))
         elsif headers.has_key?(:submitted_timestamp)
-          ts_val = headers[:submitted_timestamp]
+          return(parse_submitted_at(headers[:submitted_timestamp]))
         end
-        ts_val
+        nil
+      end
+
+      def parse_submitted_at(val)
+        if val.kind_of?(Time)
+          return val
+        end
+        ActiveSupport::TimeZone.new("UTC").parse(val) rescue nil
       end
 
       def extract_event_name(di)
